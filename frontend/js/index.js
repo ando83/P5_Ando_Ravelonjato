8e9104859cb6f21140e0d36e2e6c8f4ab9e893b7
ando83 <ando.ravel@hotmail.com>
2020-10-28 12:06:09 +0100

    structure de la page index.html et mise en forme fait

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,92 +1,35 @@
-// Requete ajax
-
-/*const articles = document.getElementById("articles");
-      url = "http://localhost:3000/api/teddies";
-
-fetch(url)
-    .then(response => response.json())
-    .then(json =>{
-        //console.log(json)
-        json.forEach(({_id, name, description, price, imageUrl}) =>{
-            const div = document.createElement("div")
-            const h3 = document.createElement("h3")
-            const h4 = document.createElement("h4")
-            const p = document.createElement("p")
-            const img = document.createElement("img")
-            const lien = document.createElement("a")
-            
-
-            const nodeName = document.createTextNode (name)
-            const nodePrice = document.createTextNode (price + " €")
-            const nodeDescription = document.createTextNode (description)
-            img.src = imageUrl
-            lien.href = 'produit.html?id=' + _id;
-            lien.textContent = "détails";
-
-
-            articles.appendChild (div)
-            div.appendChild (h3)
-            div.appendChild (h4)
-            div.appendChild (p)
-            div.appendChild (img)
-            div.appendChild (lien)
-
-            h3.appendChild(nodeName)
-            h4.appendChild(nodePrice)
-            p.appendChild(nodeDescription)
-
-           
-        })
-    })
-    .catch(err =>{
-        alert("Le serveur ne répond pas !");
-        console.error(err)
-})*/
-
-
-
-
-
-
-
 // Requête ajax (GET) + modifier structure de la page (DOM)
 
 const article = document.getElementById("articles");
       url = "http://localhost:3000/api/teddies";
      
-
 fetch(url)
     .then(response => response.json())
     .then(json =>{
         json.forEach(({_id, name, description, price, imageUrl}) =>{
             const div = document.createElement("div");
+            div.setAttribute("class", "elements")
             const h3 = document.createElement("h3")
             const h4 = document.createElement("h4")
-            const p = document.createElement("p")
             const img = document.createElement("img")
             const lien = document.createElement("a")
             
-
-            const nodeName = document.createTextNode (name)
-            const nodePrice = document.createTextNode (price + " €")
-            const nodeDescription = document.createTextNode (description)
+            const newName = document.createTextNode (name)
+            const newPrice = document.createTextNode (price + " €")
+            const newDescription = document.createTextNode (description)
             img.src = imageUrl
             lien.href = 'produit.html?id=' + _id;
             lien.textContent = "détails";
-
-
+            
             articles.appendChild (div)
             div.appendChild (h3)
             div.appendChild (h4)
-            div.appendChild (p)
+          
             div.appendChild (img)
             div.appendChild (lien)
 
-            h3.appendChild(nodeName)
-            h4.appendChild(nodePrice)
-            p.appendChild(nodeDescription)
-
-           
+            h3.appendChild(newName)
+            h4.appendChild(newPrice)
         })
     })
     .catch(err =>{
