4931f5949a80d64028592394bf0467e7ea6e7b2e
ando83 <ando.ravel@hotmail.com>
2020-11-17 08:54:18 +0100

    récupérer les données api avec méthode fetch sur la page principale et la page produit

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,41 +1,52 @@
-// Requête ajax (GET) + modifier structure de la page (DOM)
 
-const article = document.getElementById("articles");
-      url = "http://localhost:3000/api/teddies";
-     
+// Requête API Fetch pour récupérer les données  + modifier structure de la page (DOM)
+
+let url = "http://localhost:3000/api/teddies";
+
 fetch(url)
     .then(response => response.json())
-    .then(json =>{
-        json.forEach(({_id, name, description, price, imageUrl}) =>{
-            const div = document.createElement("div");
-            div.setAttribute("class", "elements")
-            const h3 = document.createElement("h3")
-            const h4 = document.createElement("h4")
-            const img = document.createElement("img")
-            const lien = document.createElement("a")
-            
-            const newName = document.createTextNode (name)
-            const newPrice = document.createTextNode ("Prix : " + " " + price + " €")
-            const newDescription = document.createTextNode (description)
-            img.src = imageUrl
-            lien.href = 'produit.html?id=' + _id;
-            lien.textContent = "Détails";
-            
-            articles.appendChild (div)
-            div.appendChild (h3)
-            div.appendChild (h4)
-          
-            div.appendChild (img)
-            div.appendChild (lien)
+    .then(data =>{
+    for(let i = 0; i < data.length; i++){
+    
+        
+    let article = document.getElementById("articles");
+   
+     //Créer div elements
+     let divTitre = document.createElement("div");
+     divTitre.setAttribute("class", "elements");
+     article.appendChild(divTitre);
+       
+     //créer h3, le nom des oursons
+     let h3Titre = document.createElement("h3");
+     h3Titre.style.color = "rgba(255, 255, 255)";
+     h3Titre.style.fontSize = "1.8em";
+     h3Titre.style.margin = "10px 0";
+     h3Titre.innerHTML = data[i].name;
+     divTitre.appendChild (h3Titre);
 
-            h3.appendChild(newName)
-            h4.appendChild(newPrice)
-        })
-    })
-    .catch(err =>{
-        alert("Le serveur ne répond pas !");
-        console.error(err)
+    //créer h4, le prix des oursons
+    let h4Titre = document.createElement("h4");
+    h4Titre.style.fontSize = "1.3em";
+    h4Titre.style.margin = "0px 0px 10px 0px";
+    h4Titre.innerHTML = "<mark>Prix = " + data[i].price + " €</mark>";
+    divTitre.appendChild (h4Titre);
+
+    //rajouter image
+    let imgTitre = document.createElement("img");
+    imgTitre.src = data[i].imageUrl;
+    divTitre.appendChild (imgTitre);
+
+    //créer lien du produit
+    let lien = document.createElement("a");
+    lien.className ='btn';
+    lien.href = "produit.html?id=" + data[i]._id;
+    lien.innerHTML = "Voir le produit";
+    divTitre.appendChild (lien);
+    
+  }  
 })
-
-
-
+    
+     //.catch(error => console.log(error))
+     .catch((error) => {
+        console.error(error);
+      });
--- frontend/js/produit.js
+++ frontend/js/produit.js
@@ -1,2 +1,56 @@
-// Récupérer l'id du produit
+//Uliliser la méthode location search pour récupérer l'id de l'url
+
+let urlId = "http://localhost:3000/api/teddies/";
+
+function recupererId(){
+    const urlActuel = window.location.search.slice(4);// on récupére seulement l'id du produit
+    return urlActuel
+}
+
+fetch (urlId + recupererId())
+.then(response => response.json())
+.then(data=> {
+    
+    let produit = document.getElementById("produits");
+    
+    let divProduit = document.createElement("div");
+    divProduit.setAttribute("class", "elementproduit");
+    produit.appendChild(divProduit);
+
+    let h3Produit = document.createElement("h3");
+    h3Produit.style.fontSize = "1.8em";
+    h3Produit.style.margin = "10px 0";
+    h3Produit.innerHTML = data.name;
+    divProduit.appendChild(h3Produit);
+    
+    let imgProduit = document.createElement("img");
+    imgProduit.src = data.imageUrl;
+    divProduit.appendChild (imgProduit);
+
+    let h4Produit = document.createElement("h4");
+    h4Produit.style.margin = "7px 0";
+    h4Produit.innerHTML = data.description;
+    divProduit.appendChild(h4Produit);
+
+    let paraProduit = document.createElement("p");
+    paraProduit.innerHTML =  "<mark>Prix = " + data.price + " €</mark>";
+    divProduit.appendChild(paraProduit);
+
+})
+
+   .catch((error) => {
+    console.error(error);
+  });
+   
+
+
+
+
+
+
+
+
+
+
+                   
 
