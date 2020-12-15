4ed401f7df8b804d8ee5266d2f63382242014b7a
ando83 <ando.ravel@hotmail.com>
2020-12-15 14:10:07 +0100

    récupération de l'id panier de la page confirmation + créer div et paragraphe pour récaputilatif de la commande

--- frontend/js/confirmation.js
+++ frontend/js/confirmation.js
@@ -5,3 +5,27 @@ let orderData = localStorage.getItem("order");
 let contacts = JSON.parse(localStorage.getItem("contact"))
 
 let prix = localStorage.getItem("prixTotal")
+
+// récupérer l'id "panier" de la page confirmation.html 
+let confirmation = document.getElementById("nom-confirmation");
+
+//créer div conteneur
+let divConfirmation = document.createElement("div");
+divConfirmation.setAttribute("class", "element-commande");
+confirmation.appendChild(divConfirmation);
+
+
+let paraDetail = document.createElement("p");
+paraDetail .setAttribute("class", "para-detail");
+paraDetail.innerHTML = "Merci"+ " "+ "<span>"+contacts.firstName+"</span>" +", votre commande à bien été enregistrée.";
+divConfirmation.appendChild(paraDetail);
+
+let paraDetaildeux = document.createElement("p");
+paraDetaildeux .setAttribute("class", "para-detail2");
+paraDetaildeux.innerHTML = "Votre numéro de commande est le :"+ " "+ "<span>"+orderData+"</span>" +" "+ "pour un montant total de " +"<span>"+prix+"Euros"+"</span>";
+divConfirmation.appendChild(paraDetaildeux);
+
+let paraDetailtrois= document.createElement("p");
+paraDetailtrois .setAttribute("class", "para-detail");
+paraDetailtrois.innerHTML = "Vous allez récevoir un email récapitulatif de votre commande à l'adresse"+ " "+ "<span>"+contacts.email+"</span>";
+divConfirmation.appendChild(paraDetailtrois);
