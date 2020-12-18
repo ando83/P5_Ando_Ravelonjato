73eafda17b13da7fb76b582ce351ba6271f0f475
ando83 <ando.ravel@hotmail.com>
2020-12-18 09:41:36 +0100

    déclarer tous les variables + indentation

--- frontend/js/produit.js
+++ frontend/js/produit.js
@@ -1,6 +1,5 @@
 
 //l'url de l'API
-
 let urlId = "http://localhost:3000/api/teddies/";
 
 //Uliliser la méthode location search pour récupérer l'id de l'url + modifier la structure de la page produit
@@ -10,20 +9,23 @@ function recupererId(){
     return urlActuel;
 }
 
+//récupérer id "produits" de la page html produit
+let produit = document.getElementById("produits");
+
+//Déclarer les variables
+let divProduit, h3Produit, imgProduit, h4Produit, paraProduit; 
+
 fetch (urlId + recupererId())
 .then(response => response.json())
 .then(data=> {
-     
-    //récupérer id "produits" de la page html produit
-    let produit = document.getElementById("produits");
-
+console.log(data)    
     //Créer div produit
-    let divProduit = document.createElement("div");
+    divProduit = document.createElement("div");
     divProduit.setAttribute("class", "elementproduit");
     produit.appendChild(divProduit);
 
     //créer h3, le nom des oursons
-    let h3Produit = document.createElement("h3");
+    h3Produit = document.createElement("h3");
     h3Produit.style.fontSize = "1.8em";
     h3Produit.style.margin = "10px 0";
     h3Produit.style.color = "white";
@@ -31,23 +33,22 @@ fetch (urlId + recupererId())
     divProduit.appendChild(h3Produit);
 
     //rajouter image
-    let imgProduit = document.createElement("img");
+    imgProduit = document.createElement("img");
     imgProduit.src = data.imageUrl;
     divProduit.appendChild (imgProduit);
 
     //créer h4, le prix des oursons
-    let h4Produit = document.createElement("h4");
+    h4Produit = document.createElement("h4");
     h4Produit.style.margin = "7px 0";
     h4Produit.innerHTML = data.description;
     divProduit.appendChild(h4Produit);
 
     //créer un paragraphe, description des oursons
-    let paraProduit = document.createElement("p");
+    paraProduit = document.createElement("p");
     paraProduit.innerHTML =  "<mark>Prix = " + data.price + " €</mark>";
     divProduit.appendChild(paraProduit);
     
     //créer une liste pour le choix des couleurs + boucle
-         
     for(let i = 0; i < data.colors.length; i++){
         let option = document.createElement("option");
         option.innerHTML=data.colors[i];
@@ -56,19 +57,14 @@ fetch (urlId + recupererId())
     
     //Messages d'alerte lors de l'ajout du produit 
    function alertProduit(){
-        //alert("Sélectionner une couleur");
         swal("Sélectionnez une couleur!","", "warning");
     }
     
     function alertAjouter(){
-        
-        swal("Article ajouté au panier!", "cliquez sur l'icône Panier pour voir les détails", "success");
-        
+        swal("Article ajouté au panier!", "cliquez sur l'icône Panier pour voir les détails", "success"); 
     }  
     
-
     //sauvegarder le produit au clic du bouton pour mettre au panier
-
     let btn = document.getElementById("bouton_produit");
     btn.addEventListener ("click", function(event){
      event.preventDefault();  //blocage du comportement par défaut du navigateur lorsqu'un événement se produit
@@ -78,16 +74,13 @@ fetch (urlId + recupererId())
        
         if( couleurSelect == ""){ //si null, message d'alerte
             alertProduit()
-           
              
         }else{
-            
             let produitTableau = JSON.parse(localStorage.getItem('panier')) || [];//Analyser et récupérer toutes les clés via localstorage 
             produitTableau.push({id: data._id, name: data.name, price : data.price, image:data.imageUrl, colors: couleurSelect, quantité: 1}); //On ajoute des éléments au tableau
             localStorage.setItem('panier', JSON.stringify(produitTableau));//stocker clé et valeur pendant l'ajout dans le localStorage et conversion valeur javascript en chaîne JSON - via localstorage
             
             //le nombre d'article est rajouté via localStorage
-            
             let localPanier = JSON.parse(localStorage.getItem("nombrePanier"));
             localPanier = parseInt(localPanier);
             if(localPanier){
@@ -95,15 +88,10 @@ fetch (urlId + recupererId())
             }else{
                 localStorage.setItem("nombrePanier", 1); 
             }
-            
             alertAjouter();//Message d'alerte lors de l'ajout
-            
-        }
-       
+        }  
     })
-
 })
-
 .catch((error) => {
  console.error(error);
 });
