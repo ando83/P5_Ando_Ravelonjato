a935fe67fdaa0fed19d6ae68ee241651866d2c7a
ando83 <ando.ravel@hotmail.com>
2020-12-21 11:21:26 +0100

    créer deux fonctions pour la création des éléments

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -9,43 +9,53 @@ let article = document.getElementById("articles");
 //déclarer les variables
 let divTitre, h3Titre, h4Titre,imgTitre,lien;
 
+//Fonction pour simplifier la création de mes éléments
+function createElement(element) {
+    return document.createElement(element); 
+  }
+
+  function appendChild(parent, el) {
+    return parent.appendChild(el); 
+  }
+
 fetch(url)
     .then(response => response.json())
-    .then(data =>{
+    .then(function(data){
     console.log(data)
     for(let i = 0; i < data.length; i++){
     
-    //Créer div elements
-    divTitre = document.createElement("div");
+    //Créer div elements à l'aide des fonctions createElement et appendChild
+    divTitre = createElement("div")
     divTitre.setAttribute("class", "elements");
-    article.appendChild(divTitre);
-       
+    appendChild(article, divTitre)   
+
     //créer h3, le nom des oursons
-    h3Titre = document.createElement("h3");
+    h3Titre = createElement("h3")
     h3Titre.style.color = "rgba(255, 255, 255)";
     h3Titre.style.fontSize = "1.8em";
     h3Titre.style.margin = "10px 0";
     h3Titre.innerHTML = data[i].name;
-    divTitre.appendChild (h3Titre);
+    appendChild(divTitre, h3Titre);
 
     //créer h4, le prix des oursons
-    h4Titre = document.createElement("h4");
+    h4Titre = createElement("h4")
     h4Titre.style.fontSize = "1.3em";
     h4Titre.style.margin = "0px 0px 10px 0px";
     h4Titre.innerHTML = "<mark>Prix = " + data[i].price + " €</mark>";
-    divTitre.appendChild (h4Titre);
+    appendChild(divTitre, h4Titre);
 
     //rajouter image
-    imgTitre = document.createElement("img");
+    imgTitre = createElement("img")
     imgTitre.src = data[i].imageUrl;
-    divTitre.appendChild (imgTitre);
+    appendChild(divTitre, imgTitre);
 
     //créer lien du produit
-    lien = document.createElement("a");
+    lien = createElement("a")
     lien.className ='btn';
     lien.href = "produit.html?id=" + data[i]._id;
     lien.innerHTML = "Voir le produit";
-    divTitre.appendChild (lien);
+    appendChild(divTitre, lien);
+
     
        }  
    })
